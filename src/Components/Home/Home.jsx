1ab7cca8cce0fdadfff89a673705a6b277bfb9a2
Debojit <debojitganguly69@gmail.com>
2023-07-08 22:47:26 +0530

    feat:added icons in the home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import '../Home/Home.css';
+import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
 
-export default function Home(){
-    return(
-        <div className="home">
-            
-        </div>
-    );
+class Home extends Component {
+    render() {
+        return (
+            <div className="home">
+                <div className="intro">
+                    Hello! I am Debojit Ganguly
+                    <div className='icons'>
+                        <a href="https://github.com/N0vice17" target="_blank" className="github">
+                            <FaGithubSquare />
+                        </a>
+                        <a href="https://www.linkedin.com/in/debojit-ganguly-907771242/" target="_blank" className="linkedin">
+                            <FaLinkedin />
+                        </a>
+                        <a href="https://twitter.com/Debojit_18" target="_blank" className="twitter">
+                            <FaTwitterSquare />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 };
+
+export default Home
