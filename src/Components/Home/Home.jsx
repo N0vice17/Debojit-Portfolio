7b8ca355ee1529c212f30c9da169fc1ae5ffe6a8
Debojit <debojitganguly69@gmail.com>
2023-07-08 13:49:05 +0530

    fix:fixed the boroken project

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,9 +4,7 @@ import '../Home/Home.css';
 export default function Home(){
     return(
         <div className="home">
-            <div className="Debojit">
-                Hey,Debojit here
-            </div>
+            
         </div>
     );
 };
