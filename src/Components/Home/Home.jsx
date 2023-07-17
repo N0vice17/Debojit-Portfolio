7ae4b14db7d99c375cb715fb40500dd5e9344fbf
Debojit <debojitganguly69@gmail.com>
2023-07-17 22:07:57 +0530

    feat:creating workflows for deployment

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,11 +2,18 @@ import React, { Component } from "react";
 import '../Home/Home.css';
 import Typewriter from 'typewriter-effect';
 import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
-const homestring=[
+const homestring = [
     'Hello! I am Debojit Ganguly',
     'I Love to Code',
 ];
+
+// const [date, setDate] = useState(new Date());
+const date= new Date();
+// function refresh() {
+//     setDate(new Date());
+// }
 
 class Home extends Component {
     render() {
@@ -15,12 +22,15 @@ class Home extends Component {
                 <div className="intro">
                     <Typewriter
                         options={{
-                           strings:homestring,
-                           autoStart:true,
-                           loop:true,
-                           deleteSpeed:20, 
+                            strings: homestring,
+                            autoStart: true,
+                            loop: true,
+                            deleteSpeed: 20,
                         }}
                     />
+                    <div className="Clock">
+                        {date.toLocaleTimeString()}
+                    </div>
                     <div className='icons'>
                         <a href="https://github.com/N0vice17" target="_blank" className="github">
                             <FaGithubSquare />
